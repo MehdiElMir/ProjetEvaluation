91d5360240d7e292d9bb5cb269fe1beaaa088818
MehdiElMir <129956404+MehdiElMir@users.noreply.github.com>
2023-04-15 12:24:20 +0000

    adding starting files to project

---
+++ ProjectEvaluationEtudes/layout.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>admin - dashboard</title>
+    <link rel="stylesheet" href="./css/layout.css">
+</head>
+<body>
+    
+    <header>
+        <nav class="p-3 shadow-lg">
+            <a href="/">
+                <img src="./assets/logo.png" alt="logo mundiapolis" class="img-size">
+            </a>
+        </nav>
+    </header>
+
+
+    <!-- MAIN CONTENT -->
+    <main class="main">
+        <div class="sidebar">
+            <div class="sidebar-top">
+                <ul>
+                    <li><p>Enquêtes</p></li>
+                    <li><p>Professeurs</p></li>
+                    <li><p>Imprimer</p></li>
+                    <li><p>Filières</p></li>
+                    <li><p>Questionnaire</p></li>
+                    <li><p>Archive</p></li>
+                </ul>
+            </div>
+            <p class="edit">Edit</p>
+        </div>
+        <div class="main-content">
+            <div class="cards">
+                <h5>Enquête mai 2022</h3>
+                <div class="progress">
+                    <span>Finit</span>
+                    <div class="progress-bar">
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+            <div class="cards">
+                <h5>Enquête mai 2022</h3>
+                <div class="progress">
+                    <span>Finit</span>
+                    <div class="progress-bar">
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+            <div class="cards">
+                <h5>Enquête mai 2022</h3>
+                <div class="progress">
+                    <span class="progress-limit">25% (En cours)</span>
+                    <div class="progress-bar-limit">
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+            <div class="cards">
+                <h5>Enquête mai 2022</h3>
+                <div class="progress">
+                    <span class="progress-limit">25% (En cours)</span>
+                    <div class="progress-bar-limit">
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+            <div class="cards">
+                <h5>Enquête mai 2022</h3>
+                <div class="progress">
+                    <span class="progress-limit">25% (En cours)</span>
+                    <div class="progress-bar-limit">
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+            <div class="cards--plus">
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="160" height="160" fill="#989898" class="bi bi-plus" viewBox="0 0 16 16">
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                    </svg>
+                </a>
+            </div>
+            
+        </div>
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ ProjectEvaluationEtudes/login_process/config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = ''; //insert database name here!!!!!!
+$databaseUsername ='root';
+$databasePassword = '';
+
+$connexion = mysqli_connect($databaseHost, $databaseUsername,$databasePassword,$databaseName);
+ ?>
