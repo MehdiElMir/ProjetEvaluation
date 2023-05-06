abeeebb8fa5c4c289be06d587ab90a72a1add51b
MehdiElMir <m.elmir.dev@gmail.com>
2023-05-06 14:26:10 +0100

    Google authentification for admin/professor

--- ProjectEvaluationEtudes/login_process/login_admin.php
+++ ProjectEvaluationEtudes/login_process/login_admin.php
@@ -1,8 +1,8 @@
 <?php 
-   
+
    // Connexion à la base de données
    include 'config.php';
-
+  
    // Récupérer des informations d'authentification de l'utilisateur
    $admin_email = $_POST['email'];
    $admin_password = $_POST['password'];
@@ -21,7 +21,9 @@
        // Redirection vers la page d'accueil 
        header('Location: ../layout.php');
    } else {
-    
+
        // pas de résultat
        echo '<script>alert("Nom d\'utilisateur ou mot de passe incorrect.");</script>';
    }
+   
+   ?>
