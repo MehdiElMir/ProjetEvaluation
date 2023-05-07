9e83d35b6e9010d1ed4b6242fbf82a82626a20d9
midrissi01 <m.oulhaj_idrissi@mundiapolis.ma>
2023-05-07 01:25:47 +0100

    professeur side bar subject from db

--- ProjectEvaluationEtudes/login_process/login_teacher.php
+++ ProjectEvaluationEtudes/login_process/login_teacher.php
@@ -2,15 +2,24 @@
    
    // Connexion à la base de données
    include 'config.php';
+ 
    
    // Récupérer des informations d'authentification de l'utilisateur
    $professor_email = $_POST['email'];
    $professor_password = $_POST['password'];
+   $professor_Lname= $_POST['professor_Lname'];
+
+ 
    
    // lancer la requête SQL dans la table admin 
    $sql =mysqli_query($connexion, "SELECT * FROM `professeur` WHERE `professor_email` = '$professor_email' AND `professor_password` = '$professor_password'");
    $row = mysqli_fetch_array($sql);
    
+   /*$sql1=mysqli_query($connexion,
+   "SELECT subject_name from subject , professeur 
+   WHERE professeur.id_professeur = subject.professeur_id");
+*/
+
    // Vérification du résultat
 
    if (is_array($row)) {
--- ProjectEvaluationEtudes/prof_layout.php
+++ ProjectEvaluationEtudes/prof_layout.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php
+            session_start();
+            //date of today
+            //$dateNow = date("Y-m-d h:i:sa", strtotime("now"));
+            $id_professeur =  $_SESSION["id_professeur"] ;
+            $professor_Lname=  $_SESSION["professor_Lname"] ;
+            extract($_POST);
+            include("./login_process/config.php");
+            
+                
+?>
     
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,10 +19,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>professeur - dashboard</title>
     <link rel="stylesheet" href="./css/prof_layout.css">
+    
 
 </head>
 <body>
-    
+
+
     <header>
         
         <nav class="p-3 shadow-lg">
@@ -28,19 +41,37 @@
         
         <div class="sidebar">
             <div class="sidebar-top">
-                <ul>
-                    <li><p>matiere 1</p></li>
-                    <li><p>matiere 2</p></li>
-                    <li><p>matiere 3</p></li>
-                    <li><p>matiere 4</p></li>
-                    <li><p>matiere 5</p></li>
+          
+              
+                 
+                    <?php
+                
+                    $stql = "SELECT `subject_name`,`subject`.`id_subject`
+                    FROM `subject`, `professeur`
+                    WHERE `professeur`.`id_professeur` = $id_professeur and
+                        `subject`.`professeur_id`=$id_professeur;";
+                
+                    $result = mysqli_query($connexion, $stql);
+                    echo '<ul>';
+                    while ($res = mysqli_fetch_array($result)) {
+                        
+                   
+                        $id_subject = $res['id_subject'];
+                        $subject_name = $res['subject_name'];
+                        echo '<li><a href="/subject.php?id=' . $id_subject . '">' . $subject_name . '</a></li>';
+                    }
+                    echo '</ul>'
                     
-                </ul>
+                ?>
+                   
             </div>
            
         </div>
         
 
+       <!---------------------------------Session HELLO ---------------------------------------- -->
+      
+    
 
         <div class="main-content">
         <h3> Nom de matière:</h3>  
@@ -72,16 +103,19 @@
                 </tr>
             </tbody>
             </table>
-            <form>
+
+            <form method="post">
+
             <div class="mb-3">
                 <label for="exampleFormControlTextarea1" class="form-label">Saisir une action:</label>
                 <textarea class="form-control form-control-lg" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
 
             <div>
-            <button type="button" class="btn btn-secondary">Afficher les commentaires</button>
+            <button type="submit" name="boutton-commentaire"class="btn btn-success">Afficher les commentaires</button>
             <button type="button" class="btn btn-success">Ajouter une action</button>
             </div>
+
 </form>
 
     </main>
@@ -89,4 +123,20 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
+<?php
+
+
+
+
+if(isset($_POST['boutton-commentaire'])) {
+    header("Location: ./liste_commentaires.php");
+    exit();
+}
+
+
+
+
+
+
+?>
 </html>
